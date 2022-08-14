1c447e39e1b023e59d7df86f5c9517475f0f68f2
amiri <aminamiri144@gmail.com>
2022-08-14 10:33:30 +0430

    startapp and first tmplates files

--- khamka/urls.py
+++ khamka/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.shortcuts import render
+
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context=context)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ]
