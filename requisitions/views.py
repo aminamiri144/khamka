fb1c9e15f39136d5d3658bb98cb96d9323dd8f42
amiri <aminamiri144@gmail.com>
2022-08-14 11:40:55 +0430

    add some urls and path

--- requisitions/views.py
+++ requisitions/views.py
@@ -1,3 +1,11 @@
+from urllib import request
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def request_detail(request, rid):
+    return HttpResponse(f"test is {rid}")
+
+
+def requests_archive(request, year=None, month=None, day=None):
+    return HttpResponse(f'load the archive of {year}/{month}/{day}')
