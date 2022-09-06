2e00e9c55fe370b85a2c408e31a0b2a4389fe46e
amiri <aminamiri144@gmail.com>
2022-09-06 13:19:52 +0430

    add paginations (not complete)

--- customers/views.py
+++ customers/views.py
@@ -8,16 +8,19 @@ from customers.forms import CustomerForm
 
 # Create your views here.
 class CustomerListView(LoginRequiredMixin, ListView):
+    paginate_by = 3
     model = Customer
     context_object_name = 'customers'
     template_name = 'customers/list.html'
     
 
-class CustomerCreateView(LoginRequiredMixin, CreateView):
+class CustomerCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Customer
     template_name = 'customers/create.html'
     success_url = '/customers/'
     form_class = CustomerForm
+    success_message = "درخواست دهنده جدید با موفقیت افزوده شد !"
+
 
 
 class CustomerUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
