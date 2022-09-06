d681ef12da56d48e880488d8e90f2ad2314ddf0a
amiri <aminamiri144@gmail.com>
2022-09-06 12:35:54 +0430

    add allerts to project

--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,11 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
-from khamka.views import LoginRequiredMixin
+from khamka.views import LoginRequiredMixin, SuccessMessageMixin
 from customers.models import Customer
 from customers.forms import CustomerForm
+
+
+
 # Create your views here.
 class CustomerListView(LoginRequiredMixin, ListView):
     model = Customer
@@ -17,8 +20,9 @@ class CustomerCreateView(LoginRequiredMixin, CreateView):
     form_class = CustomerForm
 
 
-class CustomerUpdateView(LoginRequiredMixin, UpdateView):
+class CustomerUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Customer
     form_class = CustomerForm
     template_name = 'customers/update.html'
     success_url = '/customers/'
+    success_message = "درخواست دهنده مورد نظر با موفقیت ویرایش شد."
--- khamka/views.py
+++ khamka/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-
-
+from django.contrib import messages
+from django.shortcuts import render
 
 
 class LoginRequiredMixin(object):
@@ -19,6 +19,27 @@ class LoginRequiredMixin(object):
             return login_required(view)
         else:
             return view
+
+
+
+class SuccessMessageMixin:
+    """
+    Add a success message on successful form submission.
+    """
+    success_message = ''
+    css = ''
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        success_message = self.get_success_message(form.cleaned_data)
+        if success_message:
+            messages.success(self.request, success_message)
+        return response
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % cleaned_data
+
+
 
 
 def index(request):
