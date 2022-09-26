60549abd23ee3178ae6d292cd6d00c3c77ed6c49
amiri <aminamiri144@gmail.com>
2022-09-26 17:38:00 +0330

    complete request advance search and sort box

--- requisitions/views.py
+++ requisitions/views.py
@@ -20,12 +20,27 @@ class RequestListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         value = self.request.GET.get('q', '')
         option = self.request.GET.get('option', '')
-        query = {f'{option}__icontains': value}
-        if value:
-            object_list = self.model.objects.filter(**query)
+        status = self.request.GET.get('status', '')
+        if status and status != '0':
+            object_list = self.model.objects.filter(status=status)
+            return object_list
         else:
-            object_list = self.model.objects.all()
-        return object_list
+            query = {f'{option}__icontains': value}
+            if value:
+                object_list = self.model.objects.filter(**query)
+                self.request.session['search'] = self.request.GET.get('q','')
+            else:
+                object_list = self.model.objects.all()
+
+            return object_list
+        
+    def get_initial(self):
+        """
+        در اینجا مقدار فیلد درخواست دهنده را با توجه به ادرس مقدار دهی میکنیم 
+        """
+        if self.kwargs['option']:
+            option = self.kwargs['option']
+            return {'option': option}
     
     
 
