57fd1c284b46e1640fbd171e0f4e54134d1bab97
amiri <aminamiri144@gmail.com>
2023-01-23 15:19:34 +0330

    change settings file server env

--- khamka/settings.py
+++ khamka/settings.py
@@ -21,7 +21,7 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, 'develop.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, 'server.env'))
 
 
 IS_IN_SERVER = False if env('IIS') == 'false' else True
